521aeab3c29357d773cbd74e8186d8ce83803bd4
samuel-prata <samuel.silverio001@hotmail.com>
2025-03-24 17:52:15 -0300

    feat/samu: add header and texts in home screen

--- src/app/(pages)/home/page.jsx
+++ src/app/(pages)/home/page.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import Header from '../../components/header/index.jsx'
 export default function page() {
   return (
-    <Header/>
+    <section className='bg-white w-full h-screen'>
+      <Header/>
+      <article>
+        <h2 className='text-black font-bold text-3xl border-8 w-[60%]'>Seu Site Profissional Pronto para <span className='text-5xl'>Vender Mais!</span></h2>
+        <p className='text-black text-l w-[60%]'>Acreditamos que um site bem estruturado é essencial para o sucesso do seu negócio. Oferecemos soluções personalizadas, responsivas e de alto desempenho para destacar sua marca no mercado. EleveProntos
+        Original credibilidade e conquiste mais clientes com um site desenvolvido para resultados</p>
+      </article>
+    </section>
+    
   )
 }

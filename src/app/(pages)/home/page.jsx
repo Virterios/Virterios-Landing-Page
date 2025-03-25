7f5433fdc742a270419ea4ee8d179ce8f37506a6
samuel-prata <samuel.silverio001@hotmail.com>
2025-03-25 18:58:07 -0300

    feat/samu: style header and first section

--- src/app/(pages)/home/page.jsx
+++ src/app/(pages)/home/page.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from '../../components/header/index.jsx'
 export default function page() {
   return (
-    <section className='bg-white w-full h-screen'>
+    <section className="bg-[url('/Virterios-lp.png')] bg-cover bg-center h-screen w-full">
       <Header/>
-      <article>
-        <h2 className='text-black font-bold text-3xl border-8 w-[60%]'>Seu Site Profissional Pronto para <span className='text-5xl'>Vender Mais!</span></h2>
-        <p className='text-black text-l w-[60%]'>Acreditamos que um site bem estruturado é essencial para o sucesso do seu negócio. Oferecemos soluções personalizadas, responsivas e de alto desempenho para destacar sua marca no mercado. EleveProntos
+      <article className='pl-25  h-[65%] w-[60%] flex flex-col justify-evenly'>
+        <h2 className='text-white font-bold text-3xl w-[60%]'>Seu Site Profissional Pronto para <span className='text-5xl'>Vender Mais!</span></h2>
+        <p className='text-white text-l w-[60%]'>Acreditamos que um site bem estruturado é essencial para o sucesso do seu negócio. Oferecemos soluções personalizadas, responsivas e de alto desempenho para destacar sua marca no mercado. EleveProntos
         Original credibilidade e conquiste mais clientes com um site desenvolvido para resultados</p>
+        <button className='bg-[#F19F01] w-65 h-15 rounded-[6px] font-bold'>Solicite seu site agora!</button>
       </article>
     </section>
     

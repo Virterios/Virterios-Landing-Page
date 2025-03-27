4a944e5a992f558bd3fde386fe42ec197ae1a0c1
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-03-27 17:21:35 -0300

    refactor: translate directory name to english

--- src/app/(pages)/home/page.jsx
+++ src/app/(pages)/home/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../../components/header/index.jsx";
 import ServiceSection from "@/app/components/ServiceSection";
 import FirstSection from "@/app/components/FirstSection/index.jsx"
-import QuemSomos from '../../components/QuemSomos/index.jsx'
+import QuemSomos from '../../components/whoWeAre/index.jsx'
 
 export default function page() {
   return (

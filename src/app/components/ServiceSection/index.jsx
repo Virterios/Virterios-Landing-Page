136646955e189390a1fa3171d2e5d383d8292504
kleber-matos <klebermatos2019@gmail.com>
2025-03-28 12:32:08 -0300

    feat: add main title to 'ServiceSection'

--- src/app/components/ServiceSection/index.jsx
+++ src/app/components/ServiceSection/index.jsx
@@ -2,20 +2,22 @@ import ServiceCard from "../UI/ServiceCard";
 
 export default function index() {
   return (
-    <>
-      {/* <div className="text-red-500">
-        <h2>
-          Se tem problema, tem solução
-          <span>Onde tem solução, tem Virterios</span>
-        </h2>
-        <p>
-          De cada esquina às maiores redes varejistas, nós estamos lá.Somos a
-          resposta confiável que você precisa para prosperar.Seja qual for a
-          necessidade: tem solução pra tudo, tem Linx pra tudo.
-        </p>
-      </div> */}
+    <div className="h-screen">
+      <div className="mt-5 mb-5 h-5/12 flex justify-center items-center">
+        <div className="w-6/12 text-center">
+          <h2 className="text-2xl  lg:text-5xl font-semibold flex flex-col text-[#FF7F00] mb-4">
+            Se tem problema, tem solução
+            <span>Onde tem solução, tem Virterios</span>
+          </h2>
+          <p className="text-[#A4A4A4] text-sm lg:text-lg">
+            De cada esquina às maiores redes varejistas, nós estamos lá.Somos a
+            resposta confiável que você precisa para prosperar.Seja qual for a
+            necessidade: tem solução pra tudo, tem Linx pra tudo.
+          </p>
+        </div>
+      </div>
 
-      <section className="flex flex-col lg:flex-row justify-center items-center lg:h-screen">
+      <section className="flex flex-col lg:flex-row justify-center items-center">
         <ServiceCard
           title="E-commerce"
           image="/supermarket.jpg"
@@ -32,6 +34,6 @@ export default function index() {
           link="/ecommerce"
         />
       </section>
-    </>
+    </div>
   );
 }
